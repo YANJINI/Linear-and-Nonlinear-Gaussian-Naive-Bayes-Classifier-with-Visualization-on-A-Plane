4e3b463bbcb3b475b55e5814a2b84dec22c23fc4
Yeongjin Song <jinijani@Yeongjins-MacBook-Air.local>
2022-07-02 10:32:51 +0900

    examples added

---
+++ examples.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.extend(['/Users/jinijani/PycharmProjects/git_project/ML/Linear-and-Nonlinear-Gaussian-Naive-Bayes-Classifier-with-Visualization-on-A-Plane'])
+
+from GaussianNaiveBayes import twoD_coordinates_GNB
+
+a = twoD_coordinates_GNB()
